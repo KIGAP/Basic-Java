f911c36e08ae765db61a59b69fc47967aa10a15e
Iryna Kyselova <ik793757@gmail.com>
2024-08-06 14:09:36 +0200

    Add lessons

---
+++ src/main/java/com/telran/org/Homework/HWSixCar.java
@@ -0,0 +1,20 @@
+package com.telran.org.Homework;
+
+public class HWSixCar {
+
+    String model;
+
+    double price;
+
+    public HWSixCar(String model, double price) {
+        this.model = model;
+        this.price = price;
+    }
+
+    // Метод для вывода информации об автомобиле
+    @Override
+    public String toString() {
+        return "Car - " + model + ", price = " + price;
+    }
+}
+
