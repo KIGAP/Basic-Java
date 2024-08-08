304c9c4bcc26b993297142ef9ead3cb12b0ace9d
Iryna Kyselova <ik793757@gmail.com>
2024-08-08 13:21:24 +0200

    Add lessons Eleven

--- src/main/java/com/telran/org/LessonEleven/Dog.java
+++ src/main/java/com/telran/org/LessonEleven/Dog.java
@@ -2,4 +2,29 @@ package com.telran.org.LessonEleven;
 
 public class Dog {
 
+    String name;
+
+    int age;
+
+    String breed;
+
+    //Конструктор без параметров, вызывается когда пишем new Dog()
+    //Вызывается при создании объекта
+    public Dog() {
+
     }
+
+    //this.name - обращение к полю name текущего класса
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog(String name, int age, String breed) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Incorrect age " + age);
+        }
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+}
--- src/main/java/com/telran/org/LessonEleven/DogApp.java
+++ src/main/java/com/telran/org/LessonEleven/DogApp.java
@@ -2,5 +2,22 @@ package com.telran.org.LessonEleven;
 
 public class DogApp {
 
+    public static void main(String[] args) {
+        Dog smallDog = new Dog(); //при создании объекта будет вызван конструктор Dog()
+        smallDog.age = 5;
+        smallDog.name = "Bobik";
+        smallDog.breed = "Yard";
 
+        System.out.println("Dog with name " + smallDog.name + " and age " + smallDog.age);
+
+        Dog bigDog = new Dog("Funtik");
+        System.out.println("Big dog with name " + bigDog.name);
+
+        Dog middleDog = new Dog("Tuzik", 7, "Shepard");
+        System.out.println("Middle dog with name " + middleDog.name +
+                ", age = " + middleDog.age + " , breed = " + middleDog.breed);
+
+        Dog oldDog = new Dog("Barsik", 40, "Yard");
+
+    }
 }
